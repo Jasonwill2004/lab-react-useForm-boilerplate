c543368a61bed056632d5886b3fc335383ae3e80
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-04-09 11:15:04 +0530

    done

---
+++ useform/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import FormUse from './components/formUse'
+
+function App() {
+
+  return (
+    <>
+    <FormUse/>
+    </> 
+  )
+}
+
+export default App
